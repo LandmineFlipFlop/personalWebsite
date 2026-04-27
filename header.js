79849396519333e59b40f661cef44704cbb1b54b
LandmineFlipFlop <elystanmay@gmail.com>
2026-04-27 10:43:04 -0700

    added my musings category to the main site page

--- header.js
+++ header.js
@@ -1,5 +1,5 @@
 function header() {
-    document.getElementById('header').innerHTML = '<a href="https://landmine.maykids.org/">home</a>  <a href="https://landmine.maykids.org/portfolio.html">portfolio</a> <a href="https://landmine.maykids.org/about.html">about</a>\n' +
+    document.getElementById('header').innerHTML = '<a href="https://landmine.maykids.org/" style="padding-right: 1vmin">home</a> -  <a href="https://landmine.maykids.org/portfolio.html" style="padding-inline: 1vmin">portfolio</a> - <a href="https://landmine.maykids.org/musings/directory.html" style="padding-inline: 1vmin">musings</a> - <a href="https://landmine.maykids.org/about.html" style="padding-inline: 1vmin">about</a>\n' +
         '\n' +
         '<br/><hr style="width:99%;" />'
 }
