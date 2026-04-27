699c2d0887ddda709d47ecabc3c6b17e6d0c65b4
LandmineFlipFlop <elystanmay@gmail.com>
2026-04-27 10:24:48 -0700

    sitewide formatting change fixing headers to update from a single JavaScript file

---
+++ header.js
@@ -0,0 +1,5 @@
+function header() {
+    document.getElementById('header').innerHTML = '<a href="https://landmine.maykids.org/">home</a>  <a href="https://landmine.maykids.org/portfolio.html">portfolio</a> <a href="https://landmine.maykids.org/about.html">about</a>\n' +
+        '\n' +
+        '<br/><hr style="width:99%;" />'
+}
