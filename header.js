801399a1e649c0204defc602f6b5e0e9efe3b6fe
LandmineFlipFlop <elystanmay@gmail.com>
2026-04-27 10:57:42 -0700

    minor format edit

--- header.js
+++ header.js
@@ -1,5 +1,5 @@
 function header() {
-    document.getElementById('header').innerHTML = '<a href="https://landmine.maykids.org/" style="padding-right: 1vmin">home</a> -  <a href="https://landmine.maykids.org/portfolio.html" style="padding-inline: 1vmin">portfolio</a> - <a href="https://landmine.maykids.org/personalProjects/projectList.html" style="padding-inline: 1vmin">personal projects</a> - <a href="https://landmine.maykids.org/musings/directory.html" style="padding-inline: 1vmin">musings</a> - <a href="https://landmine.maykids.org/about.html" style="padding-inline: 1vmin">about</a>\n' +
+    document.getElementById('header').innerHTML = '<a href="https://landmine.maykids.org/" style="padding-right: 1vmin; padding-left: 0.5vmin">home</a> -  <a href="https://landmine.maykids.org/portfolio.html" style="padding-inline: 1vmin">portfolio</a> - <a href="https://landmine.maykids.org/personalProjects/projectList.html" style="padding-inline: 1vmin">personal projects</a> - <a href="https://landmine.maykids.org/musings/directory.html" style="padding-inline: 1vmin">musings</a> - <a href="https://landmine.maykids.org/about.html" style="padding-inline: 1vmin">about</a>\n' +
         '\n' +
         '<br/><hr style="width:99%;" />'
 }
